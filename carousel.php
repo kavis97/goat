bbcb2e79c49dc37fef2bc394f383c5db8c386cc4
Siva Kumar <Siva.Kumar@rs-components.com>
2021-08-31 16:14:41 +0100

    Using 6 images for carousel not folder

--- carousel.php
+++ carousel.php
@@ -3,16 +3,13 @@
   <div class="carousel-inner" style=" width:100%; height: 600px !important;">
     
     <?php
-        function stringEndsWith($haystack,$needle) {
-            $expectedPosition = strlen($haystack) - strlen($needle);
-            return strripos($haystack, $needle, 0) === $expectedPosition;
-        }
-        foreach (glob("./img/carousel/*.png") as $i) {
-            $active = stringEndsWith($i, '1.png') ? 'active' : '';        
+        for ($x = 1; $x <= 6; $x++) {
+            $active = $x == 1 ? 'active' : ' ';      
             echo '<div class="carousel-item ' . ($active) . '">
-                      <img class="d-block w-100" src="' . $i . '" alt="Second slide">
+                      <img class="d-block w-100" src="./img/carousel/' . $x . '.png" alt="Second slide">
                   </div>';
-        } 
+          }
+    
     ?>
 
 </div>
