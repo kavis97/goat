8cfe753ae64da8401ec05b7e6cc572ed007f8168
Siva Kumar <Siva.Kumar@rs-components.com>
2021-09-02 15:39:27 +0100

    NO php for loops

--- carousel.php
+++ carousel.php
@@ -1,15 +1,23 @@
 
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner" style=" width:100%; height: 600px !important;">
-    
-    <?php
-        for ($x = 1; $x <= 6; $x++) {
-            $active = $x == 1 ? 'active' : ' ';      
-            echo '<div class="carousel-item ' . ($active) . '">
-                      <img class="d-block w-100" src="./img/carousel/' . $x . '.png" alt="Second slide">
-                  </div>';
-          }
-    
-    ?>
-
+    <div class="carousel-item active">
+        <img class="d-block w-100" src="./img/carousel/1.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="./img/carousel/2.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="./img/carousel/3.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="./img/carousel/4.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="./img/carousel/5.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="./img/carousel/6.png" alt="Second slide">
+    </div>
+  </div>
 </div>
